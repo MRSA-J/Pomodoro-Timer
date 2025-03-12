2c4aa8e5b81bea8fe051ff18c1fa209c1c1c4252
MRSA-J <chen_wei@alumni.brown.edu>
2025-03-12 23:39:07 +0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
-import Timer from './components/Timer';
-import { useAppContext } from './AppContext';
-import { useAuth0 } from '@auth0/auth0-react';
-import LogoutButton from './components/LogOut';
-import axios from 'axios';
-import './App.css';
+import React, { useEffect } from "react";
+import Timer from "./components/Timer";
+import { useAppContext } from "./AppContext";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "./components/LogOut";
+import axios from "axios";
+import "./App.css";
 
 function App() {
   const { setUser, setHistory } = useAppContext();
-  const { isAuthenticated, isLoading, loginWithRedirect, user:auth0User } = useAuth0()
+  const {
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    user: auth0User,
+  } = useAuth0();
 
   useEffect(() => {
     console.log("isLoading", isLoading);
@@ -20,52 +25,64 @@ function App() {
         setUser(auth0User);
 
         // Check if the user is registered in the database
-        axios.get(process.env.REACT_APP_BACKEND_URL + '/api/auth/is_user_registered', {
-          params: { email: auth0User.email }
-        })
-        .then(response => {
-          if (response.data) {
-            console.log('User is registered');
-          } else {
-            console.log('User is not registered');
-            axios.post(process.env.REACT_APP_BACKEND_URL + '/api/auth/register_user', {
-              email: auth0User.email,
-              name: auth0User.name,
-              created_at: Date.now()
-            })
-            .then(response => {
-              console.log('User registered');
-            })
-            .catch(error => {
-              console.error('Error registering user:', error);
-            });
-          }
-        })
-        .catch(error => {
-          console.error('Error fetching user:', error);
-        });
+        axios
+          .get(
+            process.env.REACT_APP_BACKEND_URL + "/api/auth/is_user_registered",
+            {
+              params: { email: auth0User.email },
+            }
+          )
+          .then((response) => {
+            if (response.data) {
+              console.log("User is registered");
+            } else {
+              console.log("User is not registered");
+              axios
+                .post(
+                  process.env.REACT_APP_BACKEND_URL + "/api/auth/register_user",
+                  {
+                    email: auth0User.email,
+                    name: auth0User.name,
+                    created_at: Date.now(),
+                  }
+                )
+                .then((response) => {
+                  console.log("User registered");
+                })
+                .catch((error) => {
+                  console.error("Error registering user:", error);
+                });
+            }
+          })
+          .catch((error) => {
+            console.error("Error fetching user:", error);
+          });
         console.log("auth0User", auth0User);
 
         // Fetch the user history from the database
-        axios.get(process.env.REACT_APP_BACKEND_URL + '/api/session/get_user_history', {
-          params: { email: auth0User.email }
-        })
-        .then(response => {
-          setHistory(response.data);
-        })
-        .catch(error => {
-          console.error('Error fetching user history:', error);
-        });
+        axios
+          .get(
+            process.env.REACT_APP_BACKEND_URL + "/api/session/get_user_history",
+            {
+              params: { email: auth0User.email },
+            }
+          )
+          .then((response) => {
+            setHistory(response.data);
+          })
+          .catch((error) => {
+            console.error("Error fetching user history:", error);
+          });
       }
     }
   }, [isAuthenticated, isLoading]);
 
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <Timer />
       {isAuthenticated && <LogoutButton />}
     </div>
-      // <History />
+    // <History />
   );
 }
 
--- frontend/src/components/Avatar/Avatar.js
+++ frontend/src/components/Avatar/Avatar.js
@@ -1,18 +1,22 @@
 // frontend/src/components/Avatar.js
-import React from 'react';
-import { useAppContext } from '../../AppContext';
-import './Avatar.css';
+import React from "react";
+import { useAppContext } from "../../AppContext";
+import "./Avatar.css";
 
 const Avatar = () => {
-    const { user } = useAppContext();
-    return (
-        <img 
-            className='avatar-image'
-            src={user.picture}
-            alt="User Avatar" 
-            referrerpolicy="no-referrer" 
-        />
-    );
+  const { user } = useAppContext();
+  return (
+    <img
+      className="avatar-image"
+      src={
+        user
+          ? user.picture
+          : "https://th.bing.com/th/id/OIP.Sdwk-7MkBK1c_ap_eGCwxwHaHa?rs=1&pid=ImgDetMain"
+      }
+      alt="User Avatar"
+      referrerpolicy="no-referrer"
+    />
+  );
 };
 
 export default Avatar;
